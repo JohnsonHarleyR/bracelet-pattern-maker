8eb42e388fa0d76b317ba2b612c9df573e013fef
Harley Johnson <hjohnson@uwm.com>
2022-07-15 14:49:21 -0400

    Created the skeleton of the main component. Set it up with a css file. Placed the main component inside of App.js.

---
+++ src/components/Maker/Maker.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './resources/styling/maker.css';
+
+const Maker = () => {
+
+  return (
+    <div>
+      Maker stuff here
+    </div>
+  );
+}
+
+export default Maker;
