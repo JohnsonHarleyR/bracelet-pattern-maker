518f985d1c23700118d453e2ddb296cd0c410c27
Harley Johnson <hjohnson@uwm.com>
2022-07-15 15:24:21 -0400

    Set up all the dimensions and filenames for the images inside constants.

--- src/MakerContext.js
+++ src/MakerContext.js
@@ -1,12 +1,24 @@
-import React, {useState, createContext} from 'react';
+import React, {useState, createContext, useEffect} from 'react';
 
 const MakerContext = createContext({});
 
 const MakerProvider = ({children}) => {
 
+  const [nodesAcross, setNodesAcross] = useState(4);
+  const [strandsAcross, setStrandsAcross] = useState(0);
+
+  useEffect(() => {
+    if (nodesAcross) {
+      setStrandsAcross(nodesAcross * 2);
+    }
+  }, [nodesAcross]);
+
 
     return (
-        <MakerContext.Provider value={{}}>
+        <MakerContext.Provider value={{
+          nodesAcross, strandsAcross,
+          setNodesAcross, setStrandsAcross,
+        }}>
             {children}
         </MakerContext.Provider>
     );
