496791d70666304f1518807a9fa3b9b0cb57b6bd
Harley Johnson <hjohnson@uwm.com>
2022-08-01 17:46:58 -0400

    There is now an assortment of preset colors to choose from.

--- src/components/Maker/resources/constants/designConstants.js
+++ src/components/Maker/resources/constants/designConstants.js
@@ -7,6 +7,13 @@ export const ColorValue = {
 export const PresetHexValues = [
   ['#f7f7f7', '#8f8f8f', '#363636'],
   ['#c4a384', '#8c5f3c', '#4d300b'],
+  ['#FF8282', '#E51717', '#7D0F0F'],
+  ['#FEBD80', '#FF8B00', '#A54B00'],
+  ['#FEFEB7', '#FFFE05', '#8B8000'],
+  ['#CDFF97', '#71DA00', '#386B00'],
+  ['#9AFFEB', '#01E0C1', '#006D68'],
+  ['#9ED6FF', '#3463F1', '#0C2E96'],
+  ['#DBA4FF', '#9025F9', '#3E067C'],
 ];
 
 export const PatternDefaults = {
